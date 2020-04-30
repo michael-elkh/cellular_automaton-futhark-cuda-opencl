518d2b82f662af920f7dc80b158c90371c97e05e
El Kharroubi Michaël <michael.el-kharroubi@etu.hesge.ch>
2020-04-30 10:17:07 +0200

    In three words I can sum up everything Ive learned about life: it goes on. - Robert Frost

--- fut_bench.c
+++ fut_bench.c
@@ -5,9 +5,14 @@
 #include <stdio.h>
 #include <time.h>
 
-void init_futhark();
+extern void init_futhark();
+extern void prepare_args(bool *pixels, uint32_t width, uint32_t height);
+extern void execute();
+extern void get_result();
+extern void clean_args();
 extern void render(bool *pixels, uint32_t width, uint32_t height);
-void destroy_futhark();
+extern void destroy_futhark();
+
 
 void main(int argc, const char *argv[])
 {
@@ -37,14 +42,17 @@ void main(int argc, const char *argv[])
     double seconds_elapsed = 0.0;
 
     clock_gettime(CLOCK_MONOTONIC, &start);
+    prepare_args(src, width, height);
     for (uint32_t i = 0; i < iteration; i++)
     {
-        render(src, width, height);   
+        execute();  
     }
+    get_result();
     clock_gettime(CLOCK_MONOTONIC, &finish);
+    clean_args();
     seconds_elapsed += (double)(finish.tv_sec - start.tv_sec) + (finish.tv_nsec - start.tv_nsec) / 1.0e9;
     printf("Result: %lf\n", seconds_elapsed);
-    
+
     destroy_futhark();
     free(src);
 }
--- fut_wrap.c
+++ fut_wrap.c
@@ -5,24 +5,45 @@
 static struct futhark_context_config *f_cfg;
 static struct futhark_context *f_ctx;
 
+static struct futhark_bool_2d *in;
+static struct futhark_bool_2d *out;
+static bool *data;
+
 void init_futhark()
 {
     f_cfg = futhark_context_config_new();
     f_ctx = futhark_context_new(f_cfg);
 }
 
-void render(bool *pixels, uint32_t width, uint32_t height)
+void prepare_args(bool *pixels, uint32_t width, uint32_t height)
 {
-    struct futhark_bool_2d* in;
-    struct futhark_bool_2d* out;
+    data = pixels;
+    in = futhark_new_bool_2d(f_ctx, data, height, width);
+}
 
-    in = futhark_new_bool_2d(f_ctx, pixels, height, width);
+void execute()
+{
     futhark_entry_ac_parity(f_ctx, &out, in);
+}
+
+void get_result()
+{
     futhark_context_sync(f_ctx);
-    futhark_values_bool_2d(f_ctx, out, pixels);
-    
+    futhark_values_bool_2d(f_ctx, out, data);
+}
+
+void clean_args()
+{
     futhark_free_bool_2d(f_ctx, in);
     futhark_free_bool_2d(f_ctx, out);
+}
+
+void render(bool *pixels, uint32_t width, uint32_t height)
+{
+    prepare_args(pixels, width, height);
+    execute();
+    get_result();
+    clean_args();
 }
 
 void destroy_futhark()
