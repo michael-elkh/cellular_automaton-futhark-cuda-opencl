3ae3bf91566dddd3ddb69747526b6de6ea9d3507
El Kharroubi Michaël <michael.el-kharroubi@etu.hesge.ch>
2020-05-01 19:36:55 +0200

    Clean things

--- OpenCL/ac_opencl.c
+++ OpenCL/ac_opencl.c
@@ -32,6 +32,7 @@ int main(int argc, const char *argv[])
 	}
 
 	int length = width * height;
+	int size = length * sizeof(uint);
 
 	// Load the kernel source code into the array source_str
 	FILE *fp;
@@ -62,16 +63,16 @@ int main(int argc, const char *argv[])
 	// Create a command queue
 	cl_command_queue command_queue = clCreateCommandQueue(context, device_id, 0, &ret);
 
-	// Create memory buffers on the device for each vector
-	cl_mem d_src = clCreateBuffer(context, CL_MEM_READ_WRITE, length * sizeof(uint), NULL, &ret);
-	cl_mem d_dst = clCreateBuffer(context, CL_MEM_READ_WRITE, length * sizeof(uint), NULL, &ret);
+	// Create memory buffers on the device for each array
+	cl_mem d_src = clCreateBuffer(context, CL_MEM_READ_WRITE, size, NULL, &ret);
+	cl_mem d_dst = clCreateBuffer(context, CL_MEM_READ_WRITE, size, NULL, &ret);
 
 	uint *src, *dst;
-	src = malloc(length * sizeof(uint));
-	dst = malloc(length * sizeof(uint));
+	src = malloc(size);
+	dst = malloc(size);
 
-	// Copy the lists A and B to their respective memory buffers
-	ret = clEnqueueWriteBuffer(command_queue, d_src, CL_TRUE, 0, length * sizeof(uint), src, 0, NULL, NULL);
+	// Copy the src array to the device buffer d_src
+	ret = clEnqueueWriteBuffer(command_queue, d_src, CL_TRUE, 0, size, src, 0, NULL, NULL);
 
 	// Create a program from the kernel source
 	cl_program program = clCreateProgramWithSource(context, 1, (const char **)&source_str, (const size_t *)&source_size, &ret);
@@ -85,19 +86,20 @@ int main(int argc, const char *argv[])
 	// Execute the OpenCL kernel on the list
 	size_t global_item_size = ((length / 64) + ((length % 64) > 0)) * 64; // Process the entire lists
 	size_t local_item_size = 64;										  // Divide work items into groups of 64
-	
-	struct timespec start, finish;
-    double seconds_elapsed = 0.0;
 
-    clock_gettime(CLOCK_MONOTONIC, &start);
-	cl_uint2 size = {{ width, height }};
+	struct timespec start, finish;
+	double seconds_elapsed = 0.0;
+
+	clock_gettime(CLOCK_MONOTONIC, &start);
+	cl_uint2 img_size = {{width, height}};
 	cl_uint max = 1;
 	// Set the arguments of the kernel
-	ret = clSetKernelArg(kernel, 2, sizeof(cl_uint2), (void *)&size);
+	ret = clSetKernelArg(kernel, 2, sizeof(cl_uint2), (void *)&img_size);
 	ret = clSetKernelArg(kernel, 3, sizeof(cl_uint), (void *)&max);
-	
+
 	cl_mem tmp;
-	for(int i = 0; i < iteration; i++){
+	for (int i = 0; i < iteration; i++)
+	{
 		ret = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *)&d_dst);
 		ret = clSetKernelArg(kernel, 1, sizeof(cl_mem), (void *)&d_src);
 		// At the end the final result is in d_src
@@ -106,16 +108,16 @@ int main(int argc, const char *argv[])
 		// Swap dst and src
 		tmp = d_src;
 		d_src = d_dst;
-		d_dst = tmp; 
+		d_dst = tmp;
 	}
 	// At the end the final result is in d_src
 
 	/* copy src back to host */
-	ret = clEnqueueReadBuffer(command_queue, d_dst, CL_TRUE, 0, length * sizeof(uint), dst, 0, NULL, NULL);
+	ret = clEnqueueReadBuffer(command_queue, d_dst, CL_TRUE, 0, size, dst, 0, NULL, NULL);
 	clFinish(command_queue);
 	clock_gettime(CLOCK_MONOTONIC, &finish);
-    seconds_elapsed += (double)(finish.tv_sec - start.tv_sec) + (finish.tv_nsec - start.tv_nsec) / 1.0e9;
-    printf("Result: %lf\n", seconds_elapsed);
+	seconds_elapsed += (double)(finish.tv_sec - start.tv_sec) + (finish.tv_nsec - start.tv_nsec) / 1.0e9;
+	printf("Result: %lf\n", seconds_elapsed);
 
 	// Clean up
 	ret = clFinish(command_queue);
@@ -128,6 +130,6 @@ int main(int argc, const char *argv[])
 	ret = clReleaseContext(context);
 	free(source_str);
 	free(src);
-	free(dst);	
+	free(dst);
 	return EXIT_SUCCESS;
 }
