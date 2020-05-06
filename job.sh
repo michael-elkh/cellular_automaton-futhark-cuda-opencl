42b2b1cf29900fbc50d75205a701f17cc7691e47
El Kharroubi Michaël <michael.el-kharroubi@etu.hesge.ch>
2020-05-06 16:48:47 +0200

    Add baobab script

---
+++ job.sh
@@ -0,0 +1,10 @@
+#!/bin/env bash
+
+#SBATCH --partition=shared-gpu-EL7
+#SBATCH --time=00:20:00
+#SBATCH --mail-user=michael.el-kharroubi@etu.hesge.ch
+#SBATCH --mail-type=ALL
+#SBATCH --gres=gpu:1
+#SBATCH --output=benchmark-RES-ID:%J.out
+
+srun ~/baobab_python_env/bin/python benchmark.py
