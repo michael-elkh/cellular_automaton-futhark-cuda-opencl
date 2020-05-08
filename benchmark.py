5e1ad89ed19212d1b0033953a7e2ca4c91455116
El Kharroubi Michaël <36660415+michael-elkh@users.noreply.github.com>
2020-05-08 20:58:10 +0200

    Merge plots

--- benchmark.py
+++ benchmark.py
@@ -4,7 +4,7 @@ from matplotlib import pyplot as plt
 from matplotlib.pyplot import figure
 import subprocess
 
-ITERATIONS=10000
+ITERATIONS=1
 MAX_VALUE=16
 ORDERS = [512, 1024, 2048, 4096, 8192, 16384]
 BACKENDS=["cuda", "opencl"]
@@ -20,12 +20,12 @@ for backend in BACKENDS:
         print(res)
         panic("Something went wrong while creating the executables")
 
-    results[backend] = {}
     for automaton in ["parity", "cyclic"]:
-        results[backend][automaton] = {}
+        if automaton not in results:
+            results[automaton] = {}
         for program in [backend+"_bench", "futhark_{}_bench".format(backend)]:
-            name = "futhark" if "futhark" in program else "standard api" 
-            results[backend][automaton][name] = []  
+            name = program.replace("_bench", "").replace("_", " ")
+            results[automaton][name] = []
             for order in ORDERS:
                 res = subprocess.run(["./"+program, str(ITERATIONS), str(order), str(order), automaton, str(MAX_VALUE)], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
                 if res.returncode != 0:
@@ -34,37 +34,39 @@ for backend in BACKENDS:
                 out = str(res.stdout, encoding="utf8").strip()
                 float_res = float(out.split(" ")[1])
                 Gcells_by_second = (ITERATIONS*order**2)/float_res/1.0e9
-                results[backend][automaton][name].append(Gcells_by_second)
+                results[automaton][name].append(np.round(Gcells_by_second, 2))
 
 makedirs("plots", exist_ok=True)
-for backend, measures in results.items():
-    for automaton, version in measures.items():
-        labels = list(map(str, ORDERS))
-        x = np.arange(len(labels))  # the label locations
-        width = 0.4  # the width of the bars
+for automaton, measures in results.items():
+    labels = list(map(str, ORDERS))
+    x = np.arange(len(labels))  # the label locations
+    width = 0.2  # the width of the bars
 
-        fig, ax = plt.subplots(figsize=(7.5,5))
-        rects1 = ax.bar(x - width/2, np.round(version['standard api'], 2), width, label=backend)
-        rects2 = ax.bar(x + width/2, np.round(version['futhark'], 2), width, label="futhark "+backend)
+    fig, ax = plt.subplots(figsize=(13,6.5))
+    rects = []
+    pos = x - 1.5*width
+    for version, values in measures.items():
+        rects.append(ax.bar(pos, np.round(values, 2), width, label=version))
+        pos += width
+    
+    # Add some text for labels, title and custom x-axis tick labels, etc.
+    ax.set_ylabel('GCells by second')
+    ax.set_title("{0} automaton".format(automaton))
+    ax.set_xticks(x)
+    ax.set_xticklabels(labels)
+    ax.set_xlabel('Square matrix cells by side')
+    ax.margins(0.01, 0.3)
+    ax.legend()
 
-        # Add some text for labels, title and custom x-axis tick labels, etc.
-        ax.set_ylabel('GCells by second')
-        ax.set_title("{0} {1} automaton".format(backend, automaton))
-        ax.set_xticks(x)
-        ax.set_xticklabels(labels)
-        ax.set_xlabel('Square matrix cells by side')
-        ax.margins(0.01, 0.25)
-        ax.legend()
+    for rect in rects:
+        #Attach a text label above each bar in *rects*, displaying its height.
+        for r in rect:
+            height = r.get_height()
+            ax.annotate('{}'.format(height),
+                        xy=(r.get_x() + r.get_width() / 2, height),
+                        xytext=(0, 3),  # 3 points vertical offset
+                        textcoords="offset points",
+                        ha='center', va='bottom')
 
-        for rects in [rects1, rects2]:
-            #Attach a text label above each bar in *rects*, displaying its height.
-            for rect in rects:
-                height = rect.get_height()
-                ax.annotate('{}'.format(height),
-                            xy=(rect.get_x() + rect.get_width() / 2, height),
-                            xytext=(0, 3),  # 3 points vertical offset
-                            textcoords="offset points",
-                            ha='center', va='bottom')
-
-        plt.tight_layout()
-        plt.savefig("plots/{0}_{1}.svg".format(backend, automaton))
+    plt.tight_layout()
+    plt.savefig("plots/{0}.svg".format(automaton))
